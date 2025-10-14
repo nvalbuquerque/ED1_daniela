941bb0129d8992af2442318e91117704c852e858
nvalbuquerque <nathalialyra@ufpr.br>
2025-10-14 13:31:55 -0300

    Update main.c

--- trabalho1/main.c
+++ trabalho1/main.c
@@ -1,75 +1,177 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "agendamento.h"
+#include "paciente.h"
+#include "lista.h"
 
-void menu() {
-    printf("\n===== MENU PRINCIPAL =====\n");
-    printf("1. Cadastrar agendamento\n");
-    printf("2. Listar agendamentos por CPF\n");
-    printf("3. Listar agendamentos por sala\n");
-    printf("4. Remover agendamento\n");
-    printf("5. Exibir historico completo\n");
+void limparBuffer() //funçã criada para limpar o biffer do teclado, para que nn apareça caracteres indesejados
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) //ignora os caracteres ate o fim da linha
+        ;
+}
+
+void menuPrincipal()
+{
+    printf("\nCLINICA UNIVERSITARIA\n");
+    printf("1. Gerenciar Pacientes\n");
+    printf("2. Gerenciar Agendamentos\n");
     printf("0. Sair\n");
-    printf("==========================\n");
+    printf("Escolha uma opcao: ");
+}
+
+void menuPacientes()
+{
+    printf("\nMENU DE PACIENTES\n");
+    printf("1. Cadastrar paciente\n");
+    printf("2. Buscar paciente por CPF\n");
+    printf("3. Remover paciente\n");
+    printf("4. Exibir todos os pacientes\n");
+    printf("0. Voltar\n");
+    printf("Escolha uma opcao: ");
+}
+
+void menuAgendamentos()
+{
+    printf("\nMENU DE AGENDAMENTOS\n");
+    printf("1. Cadastrar agendamento\n");
+    printf("2. Listar por CPF\n");
+    printf("3. Listar por sala\n");
+    printf("4. Remover agendamento\n");
+    printf("0. Voltar\n");
     printf("Escolha uma opcao: ");
 }
 
 int main() {
+    ListaPacientes *listaPacientes = criarListaPacientes();
     ListaCabecalho listaAgendamentos;
-    inicializarListaCabecalho(&listaAgendamentos);  // inicializa a lista com cabeçalho
-    int opcao;
-    char cpf[15];
-    char sala[10];
-    char data[11];
+    inicializarListaCabecalho(&listaAgendamentos);
+
+    int opcaoPrincipal = -1, opcaoSub = -1;
+    char entrada[20]; // buffer para ler a entrada do teclado
+    char nome[100], cpf[20], grr[15], curso[50];
+    char sala[15], data[12];
 
     do {
-        menu();
-        scanf("%d", &opcao);
-        getchar(); // consumir \n
+        menuPrincipal();
+        if (fgets(entrada, sizeof(entrada), stdin) == NULL)
+            continue;
+        sscanf(entrada, "%d", &opcaoPrincipal);
+        switch (opcaoPrincipal)
+        {
+        case 1: // para gerenciar pacientes
+            do {
+                menuPacientes();
+                if (fgets(entrada, sizeof(entrada), stdin) == NULL)
+                    continue;
+                sscanf(entrada, "%d", &opcaoSub);
+                switch (opcaoSub)
+                {
+                case 1:
+                    printf("\nCADASTRO DE PACIENTE\n");
+                    printf("Nome: ");
+                    fgets(nome, sizeof(nome), stdin);
+                    nome[strcspn(nome, "\n")] = 0;
+                    printf("Cpf: ");
+                    fgets(cpf, sizeof(cpf), stdin);
+                    cpf[strcspn(cpf, "\n")] = 0;
+                    printf("Grr: ");
+                    fgets(grr, sizeof(grr), stdin);
+                    grr[strcspn(grr, "\n")] = 0;
+                    printf("Curso: ");
+                    fgets(curso, sizeof(curso), stdin);
+                    curso[strcspn(curso, "\n")] = 0;
+                    cadastrarPaciente(listaPacientes, nome, cpf, grr, curso);
+                    break;
+                case 2: {
+                    printf("Digite o CPF para buscar: ");
+                    fgets(cpf, sizeof(cpf), stdin);
+                    cpf[strcspn(cpf, "\n")] = 0;
+                    Paciente *p = buscarPacientePorCPF(listaPacientes, cpf);
+                    if (p)
+                        printf("Foi encontrado: %s | Cpf: %s | Grr: %s | Curso: %s\n", p->nome, p->cpf, p->grr, p->curso);
+                    else
+                        printf("Paciente nao encontrado\n");
+                    break;
+                }
+                case 3:
+                    printf("Cpf do paciente que deseja remover: ");
+                    fgets(cpf, sizeof(cpf), stdin);
+                    cpf[strcspn(cpf, "\n")] = 0;
+                    if (removerPacientePorCPF(listaPacientes, cpf))
+                        printf("Paciente removido com sucesso\n");
+                    else
+                        printf("Paciente nao encontrado\n");
+                    break;
+                case 4:
+                    exibirPacientes(listaPacientes);
+                    break;
+                }
+            } while (opcaoSub != 0);
+            break;
+        case 2: // para gerenciar agendamentos
+            do {
+                menuAgendamentos();
+                if (fgets(entrada, sizeof(entrada), stdin) == NULL)
+                    continue;
+                sscanf(entrada, "%d", &opcaoSub);
+                switch (opcaoSub)
+                {
+                case 1:
+                {
+                    printf("\nAGENDAMENTO NOVO\n");
+                    printf("Cpf do paciente: ");
+                    fgets(cpf, sizeof(cpf), stdin);
+                    cpf[strcspn(cpf, "\n")] = 0;
+                    Paciente *p = buscarPacientePorCPF(listaPacientes, cpf);
+                    if (p == NULL)
+                    {
+                        printf("Paciente nao encontrado, cadastre antes.\n");
+                        break;
+                    }
+                    cadastrarAgendamento(&listaAgendamentos, cpf);
+                    break;
+                }
+                case 2:
+                { // Listar por CPF
+                    printf("Cpf para buscar: ");
+                    fgets(cpf, sizeof(cpf), stdin);
+                    cpf[strcspn(cpf, "\n")] = 0;
+                    listarAgendamentosPorCPF(&listaAgendamentos, listaPacientes, cpf);
+                    break;
+                }
+                case 3:
+                {
+                    printf("Sala para buscar: ");
+                    fgets(sala, sizeof(sala), stdin);
+                    sala[strcspn(sala, "\n")] = 0;
+                    listarAgendamentosPorSala(&listaAgendamentos, listaPacientes, sala);
+                    break;
+                }
+                case 4:
+                    printf("Cpf: ");
+                    fgets(cpf, sizeof(cpf), stdin);
+                    cpf[strcspn(cpf, "\n")] = 0;
+                    printf("Data (DD/MM/AAAA): ");
+                    fgets(data, sizeof(data), stdin);
+                    data[strcspn(data, "\n")] = 0;
 
-        switch (opcao) {
-            case 1:
-                cadastrarAgendamento(&listaAgendamentos);
-                break;
-
-            case 2:
-                printf("\nCPF para consulta: ");
-                fgets(cpf, sizeof(cpf), stdin);
-                cpf[strcspn(cpf, "\n")] = 0;
-                listarAgendamentosPorCPF(&listaAgendamentos, cpf);
-                break;
-
-            case 3:
-                printf("\nSala para consulta: ");
-                fgets(sala, sizeof(sala), stdin);
-                sala[strcspn(sala, "\n")] = 0;
-                listarAgendamentosPorSala(&listaAgendamentos, sala);
-                break;
-
-            case 4:
-                printf("\nCPF: ");
-                fgets(cpf, sizeof(cpf), stdin);
-                cpf[strcspn(cpf, "\n")] = 0;
-                printf("Data para remover: ");
-                fgets(data, sizeof(data), stdin);
-                data[strcspn(data, "\n")] = 0;
-                removerAgendamento(&listaAgendamentos, cpf, data);
-                break;
-
-            case 5:
-                //exibirHistorico(&listaAgendamentos); TEM QUE IMPLEMENTAR AINDA
-                break;
-
-            case 0:
-                printf("\nEncerrando programa...\n");
-                break;
-
-            default:
-                printf("\nOpcao invalida!\n");
+                    if (removerAgendamento(&listaAgendamentos, cpf, data))
+                        printf("Agendamento removido com sucesso!\n");
+                    else
+                        printf("Agendamento nao encontrado.\n");
+                    break;
+                }
+            } while (opcaoSub != 0);
+            break;
         }
 
-    } while (opcao != 0);
+    } while (opcaoPrincipal != 0);
 
-    liberarListaCabecalho(&listaAgendamentos); // libera memória
+    printf("\nSaindo\n");
+    liberarListaPacientes(listaPacientes);
+    liberarListaCabecalho(&listaAgendamentos, free);
+
     return 0;
 }
